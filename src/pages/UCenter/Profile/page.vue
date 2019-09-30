14306ba3bfad3579de48d24bccd3a0568b193e2a
yuehaoa <53177683+yuehaoa@users.noreply.github.com>
2019-09-30 13:34:17 +0800

    修改个人中心页

--- src/pages/UCenter/Profile/page.vue
+++ src/pages/UCenter/Profile/page.vue
@@ -1,17 +1,35 @@
 <template>
-    <div>
-        pro
-    </div>
-</template>
-
-<script>
-export default {
-    data () {
-        return {};
-    }
-}
-</script>
-
+  <card>
+    <i-form label-position="top" style="margin:20px">
+        <formItem label="个人信息"></formItem>
+        <formItem label="昵称" >
+            <i-input style="width:700px;" type="text" size="large"/>
+        </formItem>
+        <formItem label="简介">
+            <i-input style="width:700px;" type="textarea" size="large" rows="5"/>
+        </formItem>
+        <formItem label="地址">
+            <i-input style="width:700px;" type="text" size="large"/>
+        </formItem>
+        <formItem label="职业">
+            <i-input style="width:700px;" type="text" size="large"/>
+        </formItem>
+        <formItem label="头像">
+          <i-row>
+            <i-col span="2">
+             <avatar-uploader :width="80" :height="80" :showText="false"/>
+            </i-col>
+            <i-col span="15">
+              <i-button icon="md-download" >上传头像</i-button>
+              <br/>
+              可以拖动图片到左边头像区域完成上传
+            </i-col>
+          </i-row>
+           <i-button type="success" style="margin-top:10px">确认更新</i-button>
+        </formItem>
+      </i-form>
+    </card>
+  </template>
+<script></script>
 <style lang="less">
-
 </style>
